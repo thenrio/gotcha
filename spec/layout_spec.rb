88c4f7f9c20d5d32e24075a4e818b10d40e4f006
thierryhenrio <thierry.henrio@gmail.com>
2009-11-11 23:23:41 +0100

    break solve specification : what block should be called + how it is called

--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -18,17 +18,25 @@ end
 describe "Layout.solve" do
   before do
     @layout = Layout.new
-    @layout.get('a:*:*:<2') {"version=#{version}"}
-    @layout.get('b:*:*:*') {"group=#{group}"}
-    @layout.get('c:*:*:*') {"id=#{id}"}
-    @layout.get('a:*:*:*') {"type=#{type}"}
   end
 
-  it "should return evaluation of first block that matches" do
-    @layout.solve('a:i:n:1').should == 'version=1'
-    @layout.solve('a:i:n:2').should == 'type=n'
-    @layout.solve('b:a:t:s').should == 'group=b'
-    @layout.solve('c:a:t:s').should == 'id=a'
+  it "should return of first block that matches" do
+    # given the following rules
+    @layout.get('a:*:*:<2') {1}
+    @layout.get('b:*:*:*') {2}
+    @layout.get('c:*:*:*') {3}
+    @layout.get('a:*:*:*') {4}
+
+    @layout.solve('a:i:n:1').should == 1
+    @layout.solve('b:a:t:s').should == 2
+    @layout.solve('c:a:t:s').should == 3
+    @layout.solve('a:i:n:2').should == 4
+  end
+
+  it 'should evaluate matched block in artifact context' do
+    # given a rule with that requires artifact context evaluation
+    @layout.get('*:*:*:*') {"#{group} #{id} #{version}.times more than #{type}"}
+    @layout.solve('git:rocks:svn:10').should == 'git rocks 10.times more than svn'
   end
 
   it "should return nil when no match" do
