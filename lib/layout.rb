d22e61766200a023f36d4c4e9e8d6bca4a1c355e
thierryhenrio <thierry.henrio@gmail.com>
2009-11-11 15:05:19 +0100

    specify solve should return block call of first match

--- lib/layout.rb
+++ lib/layout.rb
@@ -2,16 +2,19 @@ require 'artifact_matcher'
 
 class Layout
   attr_accessor :matcher
+  attr_reader :rules
 
   def initialize
-    @matcher = ArtifactMatcher::Default.new  
-  end
-
-  def rules
-    @rules ||= []
+    @matcher = ArtifactMatcher::Default.new
+    @rules = []
   end
 
   def get(spec, &block)
     @rules.push([spec, block])
+  end
+
+  def solve(artifact)
+    tuple = @rules.find {|t| @matcher.match(t[0], artifact)}
+    return tuple[1].call if tuple
   end
 end
--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -9,28 +9,27 @@ describe "Layout" do
   it "should have default empty rules" do
     @layout.rules.should == []  
   end
-end
-
-describe "Layout.rule" do
-  before do
-    @layout = Layout.new
-  end
-
-  it "should have default empty rules" do
-    @layout.rules.should == []
-  end
-end
-
-describe "Layout.path" do
-  before do
-    @layout = Layout.new
-  end
-
-  it "should have default empty rules" do
-    @layout.rules.should == []
-  end
 
   it "should have default ArtifactMatcher" do
     @layout.matcher.should be_instance_of ArtifactMatcher::Default
+  end
+end
+
+describe "Layout.solve" do
+  before do
+    @layout = Layout.new
+    @layout.get('a:*:*:*') {1}
+    @layout.get('b:*:*:*') {2}
+    @layout.get('c:*:*:*') {3}
+  end
+
+  it "should return first block that matches" do
+    @layout.solve('a:i:n:1').should == 1
+    @layout.solve('b:a:t:s').should == 2
+    @layout.solve('c:a:t:1').should == 3
+  end
+
+  it "should return nil when no match" do
+    @layout.solve('w:h:a:t').should be_nil
   end
 end
