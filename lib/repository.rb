d2007d0acb4a01a88aad02028186dbc999c66fb5
thierryhenrio <thierry.henrio@gmail.com>
2009-11-22 22:14:11 +0100

    file instead of f

--- lib/repository.rb
+++ lib/repository.rb
@@ -15,9 +15,9 @@ class Repository
     RestClient.get(url + '/' + layout.solve(artifact))
   end
 
-  def put(artifact, f)
+  def put(artifact, file)
     artifact = Artifact.new(artifact) unless artifact.kind_of? Artifact
     FileUtils.mkdir_p(@local+'/'+File.dirname(artifact.conventional_path))
-    FileUtils.cp(f, @local+'/'+artifact.conventional_path)
+    FileUtils.cp(file, @local+'/'+artifact.conventional_path)
   end
 end
