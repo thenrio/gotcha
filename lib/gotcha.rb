eb2d1c9b0a81c98279b4c8a061993efaa890b0d1
thierryhenrio <thierry.henrio@gmail.com>
2009-11-26 00:28:22 +0100

    remove obsolete specs from Gotcha

--- lib/gotcha.rb
+++ lib/gotcha.rb
@@ -1,14 +1,3 @@
-require 'layout'
-
 class Gotcha
-  attr_accessor :url, :layout
 
-  def initialize(url = nil)
-    @url = url
-    @layout = Layout.new
-  end
-
-  def url=(url)
-    url[-1] == '/' ? self.url=(url.chop) : @url = url
-  end
 end
--- spec/gotcha_spec.rb
+++ spec/gotcha_spec.rb
@@ -1,19 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'gotcha'
 describe "Gotcha" do
-  before do
-    @gotcha = Gotcha.new
-  end
-  
-  it "should have default layout " do
-    @gotcha.layout.should be_a Layout
-  end
-
-  it "url should chop trailing /" do
-    @gotcha.url.should be_nil
-    @gotcha.url = 'foo/'
-    @gotcha.url.should == 'foo'
-    @gotcha.url = 'foo//'
-    @gotcha.url.should == 'foo'
-  end
 end
