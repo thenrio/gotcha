40e5090d753f7857099202440df6eae8482efa57
thierryhenrio <thierry.henrio@gmail.com>
2009-11-26 00:59:49 +0100

    spec Gotcha.get should return artifact first repository will get

--- lib/gotcha.rb
+++ lib/gotcha.rb
@@ -1,6 +1,12 @@
 class Gotcha
-
   def repositories
     @repositories ||= []
+  end
+
+  def get(spec)
+    repositories.each do |r|
+      artifact = r.get(spec)
+      return artifact if artifact
+    end
   end
 end
--- spec/gotcha_spec.rb
+++ spec/gotcha_spec.rb
@@ -1,7 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'gotcha'
+require 'stringio'
+
 describe "Gotcha" do
   it 'should have empty repositories' do
     Gotcha.new.repositories.should == []
+  end
+end
+
+describe 'Gotcha.get' do
+  before do
+    @gotcha = Gotcha.new
+    @spec = 'g:i:t:v'
+  end
+
+  it 'should return what first repository that can get will return' do
+    # given @gotcha has two repositories, first will fail, second will get true
+    (first = mock).expects(:get).with(@spec).returns(nil)
+    (second = mock).expects(:get).with(@spec).returns(true)
+    @gotcha.repositories << first << second
+    # then
+    @gotcha.get(@spec).should == true
   end
 end
